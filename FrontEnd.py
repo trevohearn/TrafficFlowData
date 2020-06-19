da8fb4b1103c0365c8b44c0dcc06ee983189c9f5
Trevor O'Hearn <trevohearn@gmail.com>
2020-06-18 20:37:35 -0400

    added trafficflowdata analysis AR

--- FrontEnd.py
+++ FrontEnd.py
@@ -13,10 +13,27 @@ from dash.dependencies import Input, Output
 #plotly
 import plotly.graph_objects as go
 
+import pandas as pd
+from statsmodels.tsa.ar_model import AR, ARResults
+
+
+df = pd.read_csv('test_df_w_timeshift.csv')
+df['Date_IntervalStart'] = pd.to_datetime(df['Date_IntervalStart'])
+
+
+
 app = dash.Dash(__name__)
 app.layout = html.Div([ #container
     #top
-    html.Div([], style={
+    html.Div([html.H2('Traffic Flow Data',
+                style={
+                    'font-size' : '20px',
+                    'padding-left' : '25%',
+                    'padding-right' : '25%',
+                    'text-align' : 'center'
+                        }
+                    )
+    ], style={
       'background-color' : 'red',
       'padding' : '10px',
       'position' : 'fixed',
@@ -26,11 +43,23 @@ app.layout = html.Div([ #container
     }
     ),
     #left bar
-    html.Div([dcc.Dropdown(id='drop_type', options=[
-                {'label' : 'AR', 'value' : 'AR'},
-                {'label' : 'ARIMA', 'value' : 'ARIMA'},
-                {'label' : 'SARIMAX', 'value' : 'SARIMAX'}
-                ], value='test')
+    html.Div([
+    html.P('Select a Start Date', style={'text-align' : 'center'}),
+    dcc.Dropdown(id='startdate', value=df.index[0]),
+    html.P('Select an End Date', style={'text-align' : 'center'}),
+    dcc.Dropdown(id='enddate', value=df.index[-1]),
+    html.P('Select a Model', style={'text-align' : 'center'}),
+    dcc.Dropdown(id='drop_type', options=[
+            {'label' : 'AR', 'value' : 'AR'},
+            {'label' : 'ARIMA', 'value' : 'ARIMA'},
+            {'label' : 'SARIMAX', 'value' : 'SARIMAX'}
+            ], value='AR'),
+    html.P('P', style={'text-align' : 'center'}),
+    dcc.Input(id='pvalue', type='number', value=1, debounce=True),
+    html.P('D', style={'text-align' : 'center'}),
+    dcc.Input(id='dvalue', type='number', value=1, debounce=True),
+    html.P('Q', style={'text-align' : 'center'}),
+    dcc.Input(id='qvalue', type='number', value=1, debounce=True)
     ],
      style={
        'height' : '100%',
@@ -41,7 +70,9 @@ app.layout = html.Div([ #container
        'left' : '10px',
        'background-color' : 'grey',
        'overflow-x' : 'hidden',
-       'padding-top' : '20px'
+       'padding-top' : '20px',
+       'padding-right' : '10px',
+       'padding-left' : '10px'
      }),
     #middle
     html.Div([#dcc.Graph(id='graph')
@@ -68,33 +99,42 @@ app.layout = html.Div([ #container
 })
 
 
-
+def makeTrace(x, y):
+    return go.Scatter(x=x, y=y)
 
 
 @app.callback(Output('graph', 'figure'),
-                [Input('drop_type', 'value')]
+                [Input('drop_type', 'value'),
+                Input('startdate', 'value'),
+                Input('enddate', 'value')]
                  #Input(, 'value')])
 )
-def choose(drop_type):
+def choose(drop_type, startdate, enddate):
+    slicedf = df.loc[startdate : enddate]
+    test_slice = df.loc[enddate:]
+    endtime = list(pd.date_range(slicedf.index[-1], periods=24, freq='H'))[-1]
+    x = [0,1,2,3,4]
     if (drop_type == 'AR'):
-        trace1 = go.Bar(x=[0,1,2,3,4], y=[1,1,1,1,1])
-        trace2 = go.Bar(x=[0,1,2,3,4], y=[2,2,2,2,2])
+        m = AR(slicedf)
+        mnolag = m.fit(method='mle', ic='aic')
+        preds = mnolag.predict(start=slicedf.index[-1],end=endtime, dynamic=False).rename('AR PREDICTIONS')
+        trace1 = makeTrace(preds.index, preds.values)
+        trace2 = makeTrace(test_slice.index, test_slice.TotalVolume)
     elif (drop_type == 'ARIMA'):
-        trace1 = go.Bar(x=[0,1,2,3,4], y=[3,3,3,3,3])
-        trace2 = go.Bar(x=[0,1,2,3,4], y=[4,4,4,4,4])
+        trace1 = makeTrace(x, [3,3,3,3,3])
+        trace2 = makeTrace(x, [4,4,4,4,4])
     elif (drop_type == 'SARIMAX'):
-        trace1 = go.Bar(x=[0,1,2,3,4], y=[5,5,5,5,5])
-        trace2 = go.Bar(x=[0,1,2,3,4], y=[6,6,6,6,6])
+        trace1 = makeTrace(x, [5,5,5,5,5])
+        trace2 = makeTrace(x, [6,6,6,6,6])
     else:
-        trace1 = go.Bar(x=[0,1,2,3,4], y=[7,7,7,7,7])
-        trace2 = go.Bar(x=[0,1,2,3,4], y=[8,8,8,8,8])
+        trace1 = makeTrace(x, [7,7,7,7,7])
+        trace2 = makeTrace(x, [8,8,8,8,8])
     return {
             'data' : [trace1, trace2],
-            'type' : 'bar',
+            'type' : 'scatter',
             'name' : drop_type,
             'layout' : go.Layout(title=drop_type, barmode='stack')
             }
-
 
 
 

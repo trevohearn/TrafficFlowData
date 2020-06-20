b98e3ac39491ec49326c0c28a8ab57dcf9f0d933
Trevor O'Hearn <trevohearn@gmail.com>
2020-06-20 13:04:44 -0400

    fix showing all graph values

--- FrontEnd.py
+++ FrontEnd.py
@@ -140,6 +140,9 @@ def choose(drop_type, startdate, enddate, pvalue, dvalue, qvalue, points):
     test_slice = df.loc[enddate:]
     endtime = list(pd.date_range(slicedf.index[-1], periods=points, freq='H'))[-1]
     x = [0,1,2,3,4]
+    trace1 = None
+    trace2 = None
+    trace3 = None
     if (drop_type == 'AR'):
         m = AR(slicedf['TotalVolume'])
         mnolag = m.fit(method='mle', ic='aic')
@@ -155,7 +158,7 @@ def choose(drop_type, startdate, enddate, pvalue, dvalue, qvalue, points):
         trace1 = makeTrace(preds.index, preds.values, 'Predictions')
         trace2 = makeTrace(test_slice.index, test_slice.TotalVolume, 'Testing Data')
         half = (slicedf.shape[0] // 400)
-        trace3 = makeTrace(slicedf.index[half:], slicedf['TotalVolume'].values[half:], 'Selected Data')
+        trace3 = makeTrace(slicedf.index[points // 2:], slicedf['TotalVolume'].values[points // 2:], 'Selected Data')
     elif (drop_type == 'SARIMAX'):
         trace1 = makeTrace(x, [5,5,5,5,5], '5', 'Predictions')
         trace2 = makeTrace(x, [6,6,6,6,6], '6', 'Testing Data')
